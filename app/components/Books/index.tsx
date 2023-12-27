bf34e32051b51369a5b39c7d7e1b2dd86a97e2b2
RubsRafa <rubiarafaela2000@gmail.com>
2023-12-27 19:22:07 -0300

    refactor: adjust book component to receive dynamic content

--- app/components/Books/index.tsx
+++ app/components/Books/index.tsx
@@ -1,19 +1,46 @@
-/* eslint-disable react/no-unescaped-entities */
+'use client'
+
 import Image from 'next/image'
 import style from './style.module.css'
+import { useEffect, useState } from 'react'
+import { getListBooks } from '@/app/services/bookApi'
+import { BookType } from './types'
+import Link from 'next/link'
 
 const BooksContent = () => {
+  const [books, setBooks] = useState<BookType[]>([])
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      try {
+        const data = getListBooks()
+        setBooks(await data)
+        console.log(await data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    fetchBooks()
+  }, [])
+
   return (
     <section className={style.books__section}>
-      <div className={style.book__card}>
-        <Image className={style.book__image} src="https://potterdb.com/_next/image?url=https%3A%2F%2Fwww.wizardingworld.com%2Fimages%2Fproducts%2Fbooks%2FUK%2Frectangle-1.jpg&w=1920&q=70" alt="" width={229} height={354} />
-        <div className={style.book_content__card}>
-          <h3>Harry Potter and the Philosopher's Stone</h3>
-          <h5>J. K. Rowling</h5>
-          <h5>26.6.1997</h5>
+      {books.map((book) =>
+        <div key={book.attributes.title} className={style.book__card}>
+          <Image className={style.book__image} src={book.attributes.cover} alt="" width={229} height={354} />
+          <div className={style.book_content__card}>
+            <h3>{book.attributes.title}</h3>
+            <h5>{book.attributes.author}</h5>
+            <h5>{book.attributes.release_date}</h5>
+          </div>
+          <Link href={`/books/${book.id}`}>
+            <button className={style.book__read_button}>
+              Read
+            </button>
+          </Link>
         </div>
-        <button className={style.book__read_button}>Read</button>
-      </div>
+      )}
     </section>
   )
 }
---
+++ app/components/Books/types.tsx
@@ -0,0 +1,21 @@
+export interface BookType {
+  id: string,
+  attributes: {
+    author: string,
+    cover: string,
+    dedication: string,
+    pages: number,
+    release_date: string,
+    summary: string,
+    title: string,
+    wiki: string,
+  },
+  relationships: {
+    chapters: {
+      data: {
+        id: string,
+        type: string,
+      }[]
+    }
+  }
+}
