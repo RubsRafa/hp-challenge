74db79e5a63bc6214289fbb1bc2ea291898edc05
RubsRafa <rubiarafaela2000@gmail.com>
2023-12-29 16:38:52 -0300

    feat: get book info and chapters

--- app/services/bookApi.tsx
+++ app/services/bookApi.tsx
@@ -1,3 +1,4 @@
+import { BookInfoType, ChapterType } from "../books/[book]/type";
 import { BookType } from "../components/Books/types";
 import baseURL from "./api";
 
@@ -5,4 +6,16 @@ export async function getListBooks(): Promise<BookType[]> {
   const response = await fetch(`${baseURL}/v1/books`)
   const data = await response.json()
   return data.data;
+}
+
+export async function getBookInfo(book_id: string): Promise<BookInfoType> {
+  const response = await fetch(`${baseURL}/v1/books/${book_id}`)
+  const data = await response.json()
+  return data.data
+}
+
+export async function getChapters(book_id: string): Promise<ChapterType[]> {
+  const response = await fetch(`${baseURL}v1/books/${book_id}/chapters`)
+  const data = await response.json()
+  return data.data
 }
