d3e799f92e173e47899926fa03705298dc988e66
RubsRafa <rubiarafaela2000@gmail.com>
2023-12-30 20:55:34 -0300

    feat: add house context

---
+++ app/context/HouseContext.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import React, { createContext, useState } from "react"
+
+export type House = 'gryffindor' | 'slytherin' | 'ravenclaw' | 'hufflepuff'
+
+interface HouseContextProps {
+  house: House,
+  setHouse: React.Dispatch<React.SetStateAction<House>>
+}
+
+const getStoredHouse = (): House => {
+  if (typeof window !== 'undefined') {
+    const storedHouse = localStorage.getItem('house') as House
+    return storedHouse || 'gryffindor'
+  }
+  return 'gryffindor'
+}
+
+const initialValue: HouseContextProps = {
+  house: getStoredHouse(),
+  setHouse: () => { }
+}
+
+export const HouseContext = createContext<HouseContextProps>(initialValue)
+
+export const HouseProvider = ({ children }: { children: React.ReactNode }) => {
+  const [house, setHouse] = useState<House>(getStoredHouse)
+  return (
+    <HouseContext.Provider value={{
+      house,
+      setHouse
+    }}>{children}</HouseContext.Provider>
+  )
+}
