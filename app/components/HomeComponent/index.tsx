b98aaabaf606ac8043c0ebac1411827386ac1647
RubsRafa <rubiarafaela2000@gmail.com>
2024-01-02 20:03:53 -0300

    feat: sorting hat function

--- app/components/HomeComponent/index.tsx
+++ app/components/HomeComponent/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import style from './style.module.css'
 import { House, HouseContext } from '@/app/context/HouseContext'
 import Image from 'next/image'
@@ -10,12 +10,22 @@ import Slytherin from '../../assets/slytherin.png'
 import Ravenclaw from '../../assets/ravenclaw.png'
 import Hufflepuff from '../../assets/hufflepuff.png'
 
+import SortingHat from '../../assets/sorthat.png'
+
 export default function HomeComponent() {
+  const [sort, setSort] = useState(false)
   const { house, setHouse } = useContext(HouseContext)
   const houses = ['Gryffindor', 'Slytherin', 'Ravenclaw', 'Hufflepuff']
   const housesImages = [Gryffindor, Slytherin, Ravenclaw, Hufflepuff]
+  const sortingHatPhrases = {
+    'gryffindor': 'Ahh... Very well. Courage, hmm... All right, very well. Gryffindor!',
+    'slytherin': 'Hmm, challenging... Ambition and a lot of determination. I know, Slytherin!',
+    'ravenclaw': 'Ahh... Curiosity, intelligence. I have no doubts, Ravenclaw!',
+    'hufflepuff': 'Honesty, loyalty... I know exactly what to do with you. Hufflepuff!',
+  }
 
   const sortHouse = () => {
+    setSort(true)
     const randomIndex = Math.floor(Math.random() * houses.length)
     setHouse(houses[randomIndex].toLowerCase() as House)
     return
@@ -36,15 +46,25 @@ export default function HomeComponent() {
               onClick={() => {
                 localStorage.setItem('house', house.toLowerCase())
                 setHouse(house.toLowerCase() as House)
+                setSort(false)
               }}>
-              <Image src={housesImages[index]} alt={`${house} house image`} width={421} height={421} />
+              <Image src={housesImages[index]} alt={`${house} house image`} width={200} height={200} />
             </li>
           ))}
         </ul>
       </div>
       <div className={style.sort_house}>
         <h2>Or let the Sorting Hat decide for you!</h2>
-        <button onClick={sortHouse} className={`${house}`}>Sort Me</button>
+        <div className={style.center}>
+          <div className={style.balloon}>
+            <span className={sort ? style.show : style.hidden}>{sortingHatPhrases[house]}</span>
+            <div className={style.triangle} style={{
+              display: sort ? 'block' : 'none'
+            }}></div>
+          </div>
+          <Image className={sort ? style.show : style.hidden} src={SortingHat} alt={`${house} house image`} width={421} height={421} />
+        </div>
+        <button onClick={sortHouse}>Sort Me</button>
       </div>
     </div>
   )
